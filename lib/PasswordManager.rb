8fbb6957f411234012726c4ba0fe75eacceb2a5e
Rainiugnas <aqdtvz9@gmail.com>
2018-10-11 16:10:34 +0200

    Fix basic rubocop errors

--- lib/PasswordManager.rb
+++ lib/PasswordManager.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'PasswordManager/version'
 
 require 'PasswordManager/crypter'
--- spec/PasswordManager_spec.rb
+++ spec/PasswordManager_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe PasswordManager do
   it "has a version number" do
     expect(PasswordManager::VERSION).not_to be nil
