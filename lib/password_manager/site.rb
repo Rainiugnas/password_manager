250513ac7e28b36b24cbf348e2913e0cbee2e139
Rainiugnas <aqdtvz9@gmail.com>
2018-10-25 12:00:36 +0200

    1.1.0 - add extra attributes to site

--- lib/password_manager/site.rb
+++ lib/password_manager/site.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
 module PasswordManager
-  # Model for site data
+  # Model for site data.
   # @attr_reader [String] name The site name, also the key use to store the site (required)
   # @attr_reader [String] user The username associated to the site (required)
   # @attr_reader [String] password The password associated to the site
+  # @attr_reader [Hash(String)] extra Any other data related to the site as email or note
   # @attr_reader [String] error Hold the validation error message (nil if no error)
   # @attr_reader [Boolean] success True if the validation success else false
   class Site
-    attr_reader :name, :user, :password
+    attr_reader :name, :user, :password, :extra
     attr_reader :success, :error
 
     # Initialize the attributes and check their validity
-    def initialize name, user, password
+    # @param [String] name
+    # @param [String] user
+    # @param [String] password
+    # @param [Hash(String)] extra
+    def initialize name, user, password, extra = {}
       @name = name
       @user = user
       @password = password
+      @extra = extra
 
       @success = true
       valid_name!
--- spec/lib/password_manager/site_spec.rb
+++ spec/lib/password_manager/site_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe PasswordManager::Site do
   let(:name) { 'site name' }
   let(:user) { 'user name' }
   let(:password) { 'user password' }
-  let(:site) { described_class.new name, user, password }
+  let(:extra) { { 'extra1' => '1', 'extra2' => '2' } }
+  let(:site) { described_class.new name, user, password, extra }
 
   describe 'constructor' do
     it 'should set the attributes with the given values' do
@@ -15,45 +16,38 @@ RSpec.describe PasswordManager::Site do
       expect(site.password).to eq password
     end
 
+    it 'should set the extra attributes' do
+      expect(site.extra['extra1']).to eq '1'
+      expect(site.extra['extra2']).to eq '2'
+    end
+  end
+
+  describe 'validation' do
     context 'when all value filled' do
       it 'should success' do
         expect(site.success).to be_truthy
       end
     end
 
-    context 'when site name is empty' do
-      let(:name) { '' }
+    context 'when name is not present' do
+      it 'should fail' do
+        ['', nil].each do |name|
+          site = described_class.new name, user, password
 
-      it 'should not success' do
-        expect(site.success).to be_falsy
-        expect(site.error).to eq 'Error: site name must be present'
+          expect(site.success).to be_falsy
+          expect(site.error).to eq 'Error: site name must be present'
+        end
       end
     end
 
-    context 'when site name is nil' do
-      let(:name) { nil }
+    context 'when user is not present' do
+      it 'should fail' do
+        ['', nil].each do |user|
+          site = described_class.new name, user, password
 
-      it 'should not success' do
-        expect(site.success).to be_falsy
-        expect(site.error).to eq 'Error: site name must be present'
-      end
-    end
-
-    context 'when user name is empty' do
-      let(:user) { '' }
-
-      it 'should not success' do
-        expect(site.success).to be_falsy
-        expect(site.error).to eq 'Error: user name must be present'
-      end
-    end
-
-    context 'when user name is nil' do
-      let(:user) { nil }
-
-      it 'should not success' do
-        expect(site.success).to be_falsy
-        expect(site.error).to eq 'Error: user name must be present'
+          expect(site.success).to be_falsy
+          expect(site.error).to eq 'Error: user name must be present'
+        end
       end
     end
   end
